59981ae6e4aa80b2d2b06ae092db632b89606c96
Eugeni Bykovski <bykovskieugeni@mail.ru>
2021-02-10 14:57:03 +0300

    created react app

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-  <h1>Hello Worlds!</h1>,
+  <h1>Hello World!</h1>,
   document.getElementById('root')
 );
 
